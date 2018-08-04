938b5a8f74b17f16c11c0e099f7973d1d0dfd1a6
dergo <dergo@DESKTOP-04ODDV9>
2018-08-04 14:03:10 -0300

    Intento 2

--- Restfull.API/src/main/java/controller/CotizacionController.java
+++ Restfull.API/src/main/java/controller/CotizacionController.java
@@ -20,10 +20,10 @@ public class CotizacionController
 	 * @param MONEDA 
 	 * @return Cotizacion de la MONEDA en Pesos
 	 */
-	@RequestMapping(value="/MONEDA", method=RequestMethod.GET)
+	@RequestMapping(value="/MONEDA/quote", method=RequestMethod.GET)
 	public double cotizar(@PathVariable String MONEDA)
 	{
-		System.out.println("Cotizando el "+MONEDA);
+		System.out.println("Cotizando: "+MONEDA);
 		if(MONEDA.equals("Dolar"))
 		{
 			cotizador = new CotizadorDolar();
@@ -38,5 +38,11 @@ public class CotizacionController
 		}
 		
 		return cotizador.cotizar();
+	}
+	
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String test()
+	{
+		return "TEST";
 	}
 }
