3730455c8e2238ca64f79c946cb67518656bd3d8
damian <damian.lisas@gmail.com>
2018-08-08 14:33:12 -0300

    Modificado controladores

--- restful/src/main/java/test/restful/controller/ABMController.java
+++ restful/src/main/java/test/restful/controller/ABMController.java
@@ -1,7 +1,10 @@
 package test.restful.controller;
 
-import org.json.JSONException;
-import org.json.JSONObject;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,17 +18,18 @@ import test.restful.usuario.persistencia.UsuarioDAO;
 public class ABMController 
 {
 	@RequestMapping(value = "/usuario/alta", method = RequestMethod.POST)
-	public void alta(@RequestBody JSONObject body) throws JSONException
+	public void alta(@RequestBody String body, HttpServletResponse response) throws ParseException
 	{
+		System.out.println("Dando alta a usuario");
 		Usuario u = new Usuario();
 		UsuarioDAO udao = new UsuarioDAO();
+		JSONObject jsonBody = (JSONObject) new JSONParser().parse(body);
 		
-		u.setNombre((String)body.get("nombre"));
-		u.setApellido((String)body.get("apellido"));
-		u.setEmail((String)body.get("email"));
-		u.setPassword((String)body.get("password"));
-		
-		udao.create(u);
+		u.setNombre((String)jsonBody.get("nombre"));
+		u.setApellido((String)jsonBody.get("apellido"));
+		u.setEmail((String)jsonBody.get("email"));
+		u.setPassword((String)jsonBody.get("password"));
+			
 	}
 	@RequestMapping(value = "/usuario/baja/{id}", method = RequestMethod.POST)
 	public void baja(@PathVariable Integer id)
@@ -35,7 +39,7 @@ public class ABMController
 		udao.delete(u);
 	}
 	@RequestMapping(value = "/usuario/modificaion/{id}", method = RequestMethod.POST)
-	public void modificacion(@PathVariable Integer id, @RequestBody JSONObject body) throws JSONException
+	public void modificacion(@PathVariable Integer id, @RequestBody JSONObject body)
 	{
 		UsuarioDAO udao = new UsuarioDAO();
 		Usuario u = udao.retireve(id);
