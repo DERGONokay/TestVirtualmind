4749782e09a84990c30d3ba3f041da98bd0b67a6
damian <damian.lisas@gmail.com>
2018-08-10 02:43:20 -0300

    Agrupados scripts en carpeta scripts Agregados scritps para la instalacion

--- prueba.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-sql = "INSERT INTO User (id, nombre, apellido, email, password) VALUES"
-for i in range(10):
-        num = str(i)
-        print(sql+" ("+num+",'nombre"+num+"','apellido"+num+"','email"+num+"@gmail.com','password"+num+"');")
